4c6d8a5755919b83b7b1404804c50d1517b96fba
ET Tech <erwin@entrich-technologies.com>
2020-09-08 16:15:27 +0200

    Update models.py

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ import torch.nn.init as I
 
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self, weight):
         super(Net, self).__init__()
         
         ## TODO: Define all the layers of this CNN, the only requirements are:
@@ -18,9 +18,18 @@ class Net(nn.Module):
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
+        k_height, k_width = weight.shape[2:]
+
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        self.activ = nn.ELU()
+        self.pool = nn.MaxPool2d(2,2)
+        self.drop = nn.Dropout2d(0.1)
+        # Conv2
+        self.conv2 = nn.Conv2d(32, 64, 3)
+
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -31,7 +40,15 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
         
-        
+        # prep for linear layer
+        # flatten the inputs into a vector
+        x = x.view(x.size(0), -1)
+
         # a modified x, having gone through all the layers of your model, should be returned
+
+        #print(net)
         return x
